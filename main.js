15d354ed763cc61bb3e54a04d430d9294ab70359
ka-reem <kareemalmond@gmail.com>
2023-11-24 04:28:35 +0000

    3rd person camera follows and rotates with player, player moves faster, bug when player turns south (180deg), when turning walls don't smoothly connect with eachother (large gaps)

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/j
 // import gltf files that have our renders
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
 
+// class ThirdPersonCamera {
+//   constructor(params) {
+//     this._params = params;
+//     this._camera = params.camera;
+//     this._target = params.target;
+
+//     this._currentPosition = new THREE.Vector3();
+//     this._currentLookat = new THREE.Vector3();
+//   }
+
+//   // Implement methods to calculate the ideal offset and look-at position based on your requirements.
+
+//   // Inside your third-person camera class
+// // _CalculateIdealLookat() {
+// //   // Calculate the ideal look-at position based on your requirements.
+// //   // This can depend on the player's position and rotation.
+// // }
+
+//   update(timeElapsed) {
+//     const idealOffset = this._CalculateIdealOffset();
+//     const idealLookat = this._CalculateIdealLookat();
+
+//     // Interpolate between current and ideal positions/look-at.
+//     const t = 1.0 - Math.pow(0.001, timeElapsed);
+//     this._currentPosition.lerp(idealOffset, t);
+//     this._currentLookat.lerp(idealLookat, t);
+
+//     // Set the camera's position and look-at.
+//     this._camera.position.copy(this._currentPosition);
+//     this._camera.lookAt(this._currentLookat);
+//   }
+// }
+
 class BasicWorldDemo {
   constructor() {
     this._playerRotation = 0; // keep track of players rotation so its relative to direction its moving 
@@ -28,41 +61,51 @@ class BasicWorldDemo {
       this._OnWindowResize();
     }, false);
 
-    const fov = 60; // presepective camera
+    // presepective camera
+    const fov = 60;
     const aspect = 1920 / 1080;
     const near = 1.0;
     const far = 1000.0;
     this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this._camera.position.set(35, 2, 0); // sets the cameras intial position (x,y,z)
+    // this._camera.position.set(35, 2, 0); // sets the cameras intial position (x,y,z)
+    this._camera.position.set(0,2,15) // change this position relative to where the player goes ***
 
     this._scene = new THREE.Scene(); // scene: serves as container for all our 3d objects in the world
 
     // lighting setup 
     let light = new THREE.DirectionalLight(0xFFFFFF, 1.0); // directional light created for shadows
     // decide if we need shadows or not
-    // light.position.set(20, 100, 10);
-    // light.target.position.set(0, 0, 0);
-    // light.castShadow = true;            
-    // light.shadow.bias = -0.001;
-    // light.shadow.mapSize.width = 2048;
-    // light.shadow.mapSize.height = 2048;
-    // light.shadow.camera.near = 0.1;
-    // light.shadow.camera.far = 500.0;
-    // light.shadow.camera.near = 0.5;
-    // light.shadow.camera.far = 500.0;
-    // light.shadow.camera.left = 100;
-    // light.shadow.camera.right = -100;
-    // light.shadow.camera.top = 100;
-    // light.shadow.camera.bottom = -100;
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;            
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
     this._scene.add(light);     
 
     light = new THREE.AmbientLight(0x101010); // make sure everything is lit up
     // this._scene.add(light);
 
-    const controls = new OrbitControls( // lets the user to move the cameras position using arrow or mouse controls
-      this._camera, this._threejs.domElement);
-    controls.target.set(0, 5, 0); // (x,y,z) // where camera points
-    controls.update(); 
+    // thirs person camera
+    // third person camera
+    // this._thirdPersonCamera = new ThirdPersonCamera({
+    //   camera: this._camera,
+    //   target: this._player, 
+    // });
+    
+    // orbit controls
+    // const controls = new OrbitControls( // lets the user to move the cameras position using arrow or mouse controls
+    // this._camera, this._threejs.domElement);
+    // controls.target.set(0, 5, 0); // (x,y,z) // where camera points
+    // controls.update(); 
 
     const loader = new THREE.CubeTextureLoader();
     // load a background if you want
@@ -148,41 +191,10 @@ class BasicWorldDemo {
     // box.castShadow = true;
     // box.receiveShadow = true;
     // this._scene.add(box);
-
-    // for (let x = -8; x < 8; x++) { // this creates a bunch of random boxes around the plane
-    //   for (let y = -8; y < 8; y++) {
-    //     const box = new THREE.Mesh(
-    //       new THREE.BoxGeometry(2, 2, 2),
-    //       new THREE.MeshStandardMaterial({
-    //           color: 0x808080,
-    //       }));
-    //     box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
-    //     box.castShadow = true;
-    //     box.receiveShadow = true;
-    //     this._scene.add(box);
-    //   }
-    // }
-
-    // const box = new THREE.Mesh(
-    //   new THREE.SphereGeometry(2, 32, 32),
-    //   new THREE.MeshStandardMaterial({
-    //       color: 0xFFFFFF,
-    //       wireframe: true,
-    //       wireframeLinewidth: 4,
-    //   }));
-    // box.position.set(0, 0, 0);
-    // box.castShadow = true;
-    // box.receiveShadow = true;
-    // this._scene.add(box);
-
-    // tron ************************************************
-
-    
-    
-    // *****************************************************
     this._LoadModel();
     this._RAF();
 }
+
 
 _LoadModel() {
     const loader = new GLTFLoader();
@@ -218,8 +230,6 @@ _LoadModel() {
     window.addEventListener("keydown", (e) => {
       if(e.key === 'd' || e.key === 'D'){ //|| e.key === "ArrowRight"){ // right/left arrow conflicts with orbit controls
         console.log('Key pressed:', e.key);
-        // this._player.position.x += 0.2;
-        // player.position.x += 0.1;
         this._moveRight = true;
         this._playerRotation += 90; 
         this._player.rotateY(-Math.PI / 2);
@@ -255,12 +265,20 @@ _LoadModel() {
     let yPos = this._player.position.y 
     let zPos = this._player.position.z 
 
-    // let x
+    let wallLength; 
+    let wallWidth; 
+    const wallHeight = 1.95
 
-    // if(this._playerRotation === 90 || this._playerRotation === 270)           
+    if(this._playerRotation === 90 || this._playerRotation === 270){   
+      wallLength = 0.8
+      wallWidth = 0.05   
+    } else{
+      wallLength = 0.05
+      wallWidth = 0.8
+    }
+
     
-    // const wallGeometry = new THREE.BoxGeometry(1, 1.95, 0); // (x,y,z)
-    const wallGeometry = new THREE.BoxGeometry(1, 1.95, 0); // (x,y,z)
+    const wallGeometry = new THREE.BoxGeometry(wallLength, wallHeight, wallWidth); // (x,y,z)
 
     const wallMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff }); // Adjust the color as needed
     const wall = new THREE.Mesh(wallGeometry, wallMaterial);
@@ -273,10 +291,174 @@ _LoadModel() {
 
   }
 
+  _CameraPosition(){
+
+    // CHOPPY APROACH
+    // const height = 7.5;
+    // const distanceFromPlayer = 17;
+
+    // switch (this._playerRotation){
+    //   case 0: // initial direction
+    //     this._camera.rotation.set(0, 0, 0);
+    //     this._camera.position.set(this._player.position.x, height, this._player.position.z + distanceFromPlayer ); 
+    //     break;
+    //   case 90:
+    //     this._camera.rotation.set(0, -Math.PI / 2, 0);
+    //     this._camera.position.set(this._player.position.x - distanceFromPlayer, height, this._player.position.z ); 
+    //     break;
+    //   case 180:
+    //     this._camera.rotation.set(0, -Math.PI, 0);
+    //     this._camera.position.set(this._player.position.x  , height, this._player.position.z  - distanceFromPlayer ); 
+    //     break;
+    //   case 270: // this
+    //     this._camera.rotation.set(0, Math.PI / 2, 0);
+    //     this._camera.position.set(this._player.position.x  + distanceFromPlayer , height, this._player.position.z ); 
+    //     break;
+    //   default:
+
+    // }
+
+    /****************/
+
+      const height = 7.5;
+      const distanceFromPlayer = 17;
+  
+      let targetRotation = 0;
+  
+      switch (this._playerRotation) {
+          case 0:
+              targetRotation = 0;
+              break;
+          case 90:
+              targetRotation = -Math.PI / 2;
+              break;
+          case 180:
+              targetRotation = -Math.PI;
+              break;
+          case 270:
+              targetRotation = Math.PI / 2;
+              break;
+          default:
+              targetRotation = 200;
+      }
+  
+      // linear interpolation (lerp), smoother transitions
+      this._camera.rotation.y = THREE.MathUtils.lerp(this._camera.rotation.y, targetRotation, 0.1);
+  
+      const targetPosition = new THREE.Vector3();
+  
+      switch (this._playerRotation) {
+          case 0:
+              targetPosition.copy(this._player.position).add(new THREE.Vector3(0, height, distanceFromPlayer));
+              break;
+          case 90:
+              targetPosition.copy(this._player.position).add(new THREE.Vector3(-distanceFromPlayer, height, 0));
+              break;
+          case 180: // does a 360 rotation turn before facing this way
+              targetPosition.copy(this._player.position).add(new THREE.Vector3(0, height, -distanceFromPlayer));
+              break;
+          case 270:
+              targetPosition.copy(this._player.position).add(new THREE.Vector3(distanceFromPlayer, height, 0));
+              break;
+          default:
+              targetPosition.copy(this._player.position).add(new THREE.Vector3(0, height, distanceFromPlayer));
+      }
+  
+      //  linear interpolation (lerp), smoother transitions
+      this._camera.position.lerp(targetPosition, 0.1);
+      console.log(this._playerRotation)
+  
 
 
 
-_OnWindowResize() { // handes camera adjustment and rendering when window is resized // shouldn't slow down rendering but i feel like it does
+    /******************************************** */
+    // this._camera.position.set(0,2,15) // change this position relative to where the player goes ***
+
+    // this._camera.position.set(this._player.position.x, 5, this._player.position.z + 10); // works but only follows player
+    // // this._camera.position.copy(this._player.position).add(new THREE.Vector3(0, 5, 10)).applyAxisAngle(new THREE.Vector3(0, 1, 0), this._player.rotation.y);
+
+
+    //     window.addEventListener("keydown", (e) => {
+    //   if(e.key === 'd' || e.key === 'D'){ //|| e.key === "ArrowRight"){ // right/left arrow conflicts with orbit controls
+    //     // this._camera.rotation.y += 90
+    //     // this._camera.rotation.y += 90 * Math.PI / 180
+    //     this._camera.rotateY(90 * Math.PI / 180);
+        
+    //     // this._camera.position.copy(this._player.position).add(new THREE.Vector3(0, 5, -10)).applyAxisAngle(new THREE.Vector3(0, 1, 0), this._player.rotation.y);
+
+    //   }
+
+    //   // if(e.key === 'a' || e.key === 'A'){ //|| e.key === "ArrowLeft"){
+        
+    //   // }
+
+    // });
+
+    // // this._camera.rotation.y += 90
+    // this._camera.position.set(this._player.position.x, 5, this._player.position.z + 10); 
+
+    //************************************************************************** */
+
+
+
+    // this._camera.position.set(this._player.position.x, 5, this._player.position.z + 10);
+    // this._camera.lookAt(this._player.position);
+
+ 
+    // Update the camera's look-at position to focus on the player
+    // this._camera.lookAt(this._player.position);
+    // switch (this._playerRotation){
+    //   case 0: // initial direction
+    //     this._player.position.z -= speed;
+    //     break;
+    //   case 90:
+    //     this._player.position.x += speed;
+    //     break;
+    //   case 180:
+    //     this._player.position.z += speed;
+    //     break;
+    //   case 270:
+    //     this._player.position.x -= speed;
+    //     break;
+    //   default:
+
+    // }
+    
+    
+    // if
+    // camera.lookAt(player.position);
+  
+    // Render the scene
+    // renderer.render(scene, camera);
+  
+    // Call animate again on the next frame
+    // requestAnimationFrame(this._CameraPosition);
+    
+
+    // var offset = new THREE.Vector3(this._player.position.x + 0, this._player.position.y + 6, this._player.position.z + 10);
+
+
+    // window.addEventListener("keydown", (e) => {
+    //   if(e.key === 'd' || e.key === 'D'){ //|| e.key === "ArrowRight"){ // right/left arrow conflicts with orbit controls
+    //     this._camera.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+    //   }
+
+    //   if(e.key === 'a' || e.key === 'A'){ //|| e.key === "ArrowLeft"){
+        
+    //     // this._camera.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+    //     this._camera.rotation.y += 90
+    //   }
+
+    // });
+    
+    // this._camera.position.lerp(offset, 0.2);
+  
+  }
+
+
+
+
+_OnWindowResize() { // handes camera adjustment and rendering when window is resized 
     this._camera.aspect = window.innerWidth / window.innerHeight;
     this._camera.updateProjectionMatrix();
     this._threejs.setSize(window.innerWidth, window.innerHeight);
@@ -285,22 +467,22 @@ _OnWindowResize() { // handes camera adjustment and rendering when window is res
 
 _RAF() { // render loop that continously renders the scene
     requestAnimationFrame(() => {
-      const speed = 0.4;
+      const speed = 0.8;
       this._threejs.render(this._scene, this._camera);
       this._RAF();
       this._JetWall();
+      
+      this._CameraPosition();
 
-            // Update the wall's position based on the player's movement
-            // if (this._wall && this._player) {
-              // this._wall.position.x = this._player.position.x;
-              // this._wall.position.z = this._player.position.z - 2;
-            // }
+      // controls.enabled = false; // disable oribit controls so they don't conflict with 3rd person camera
+
+      // this._thirdPersonCamera();
+
+      // this._thirdPersonCamera.update(); // Call the update method of your third-person camera.
 
       switch (this._playerRotation){
         case 0: // initial direction
           this._player.position.z -= speed;
-          // console.log(this._player.position.z);
-        
           break;
         case 90:
           this._player.position.x += speed;
